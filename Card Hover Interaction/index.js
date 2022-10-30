c2339f27d4d05495ed18938defc601359af23312
Jeda777 <jakub.jeda@wp.pl>
2022-10-30 14:26:48 +0100

    Cleaning

--- Card Hover Interaction/index.js
+++ Card Hover Interaction/index.js
@@ -1,5 +1,4 @@
 const card = document.querySelector('.card');
-const cardContent = document.querySelector('.card-content');
 card.addEventListener('touchstart', () => {
     card.classList.add('active');
     console.log('dodano');
