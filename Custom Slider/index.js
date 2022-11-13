003e8ac7252ad6d99ee425a2ff612c8327e90c16
Jeda777 <jakub.jeda@wp.pl>
2022-11-13 11:59:59 +0100

    Slider update
    Now works with keyboard

--- Custom Slider/index.js
+++ Custom Slider/index.js
@@ -1,9 +1,18 @@
 const input = document.querySelector('input');
-input.addEventListener('pointermove', (e) => {
+input.addEventListener('input', () => {
     const r = 211 + (Number(input.value) / 100) * 27;
     const g = 211 + (Number(input.value) / 100) * -34;
     const b = 211 + (Number(input.value) / 100) * -166;
     input.style.background = `rgb(${r}, ${g}, ${b})`;
     input.dataset.value = input.value;
-    input.style.setProperty('--left', `${e.pageX - input.getBoundingClientRect().left}px`);
+    const normalize = (Number(input.value) / 100) * 22.5 - 7.5;
+    if (Number(input.value) === 50) {
+        input.style.setProperty('--left', `${(input.clientWidth * Number(input.value)) / 100}px`);
+    }
+    else if (Number(input.value) >= 50) {
+        input.style.setProperty('--left', `${(input.clientWidth * Number(input.value)) / 100 - normalize}px`);
+    }
+    else if (Number(input.value) <= 50) {
+        input.style.setProperty('--left', `${(input.clientWidth * Number(input.value)) / 100 - normalize + 3}px`);
+    }
 });
