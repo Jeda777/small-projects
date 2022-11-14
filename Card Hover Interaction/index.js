29019a385e8209135a4e90af767bae1b89158cdc
Jeda777 <jakub.jeda@wp.pl>
2022-11-14 19:02:22 +0100

    Card hover update
    Devices without hover sees card always active

--- Card Hover Interaction/index.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelector('.card');
-card.addEventListener('touchstart', () => {
-    card.classList.add('active');
-    console.log('dodano');
-    setTimeout(() => {
-        card.classList.remove('active');
-        console.log('wydano');
-    }, 500);
-});
