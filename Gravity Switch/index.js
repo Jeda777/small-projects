171e0f466f1bb186363e9920bf9e2a2401f45d10
Jeda777 <jakub.jeda@wp.pl>
2022-11-14 19:13:48 +0100

    Gravity switch update
    Added keyboard support

--- Gravity Switch/index.js
+++ Gravity Switch/index.js
@@ -1,7 +1,7 @@
 const switchEl = document.getElementById('switch');
 const ball = document.getElementById('ball');
 switchEl.style.animationPlayState = 'paused';
-switchEl.addEventListener('click', () => {
+const listenerFunc = () => {
     switchEl.style.animationPlayState = 'running';
     if (switchEl.classList.length === 0) {
         switchEl.classList.add('activated');
@@ -12,5 +12,11 @@ switchEl.addEventListener('click', () => {
         switchEl.classList.toggle('deactivated');
         ball.classList.toggle('activated');
         ball.classList.toggle('deactivated');
+    }
+};
+switchEl.addEventListener('click', listenerFunc);
+switchEl.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter' || e.code === 'Space') {
+        listenerFunc();
     }
 });
